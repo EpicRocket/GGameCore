848df55af31fb536522ed12ca023f69d4dcfc780
cannot206 <editionunit@gmail.com>
2025-08-05 19:54:23 +0900

    GameState의 경우에는 PrimaryPlayerController의 생성을 받는 이벤트 추가

--- Source/GGameCore/Public/Core/Component/GGameStateComponent.h
+++ Source/GGameCore/Public/Core/Component/GGameStateComponent.h
@@ -3,10 +3,12 @@
 #pragma once
 
 #include "Core/Component/GGameCoreComponent.h"
+#include "Containers/Ticker.h"
 
 #include "GGameStateComponent.generated.h"
 
 class AGameStateBase;
+class APlayerController;
 
 UCLASS(Abstract, Blueprintable, BlueprintType, HideCategories = (Trigger, PhysicsVolume))
 class GGAMECORE_API UGGameStateComponent : public UGGameCoreComponent
@@ -14,7 +16,26 @@ class GGAMECORE_API UGGameStateComponent : public UGGameCoreComponent
 	GENERATED_BODY()
 	
 public:
+	// ActorComponent
+	virtual void InitializeComponent();
+	virtual void UninitializeComponent();
+	// ~ActorComponent
+
+public:
 	UFUNCTION(BlueprintCallable)
 	AGameStateBase* GetGameState() const;
+
+protected:
+	void OnEnterPrimaryController_Internal();
+	virtual void OnEnterPrimaryController() {}
+	UFUNCTION(BlueprintImplementableEvent, meta = (DisplayName = "OnEnterPrimaryController"))
+	void K2_OnEnterPrimaryController();
+
+public:
+	UPROPERTY(Transient, BlueprintReadOnly)
+	TWeakObjectPtr<APlayerController> PrimaryController;
+
+private:
+	FTSTicker::FDelegateHandle TickHandle;
 
 };
